b01bf2d8d862008c299c63405d4b5d4bf8d1b335
Andrei Drulchenko <andrdru@gmail.com>
2022-01-29 18:15:30 +0300

    improve template http example

--- template/internal/app_http.example.go
+++ template/internal/app_http.example.go
@@ -1,8 +1,10 @@
 package internal
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/andrdru/gosvc/apimsg"
 	m "github.com/andrdru/gosvc/middlewares"
 	"github.com/rs/zerolog"
 
@@ -29,19 +31,34 @@ func init() {
 }
 
 func handlerExample(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("hello world"))
+	var msg = apimsg.NewJson()
+
+	type response struct {
+		Hello     string `json:"hello"`
+		ParamGET  string `json:"param_get"`
+		ParamPOST string `json:"param_post"`
+	}
+
+	var resp = &response{
+		Hello: "hello world!",
+	}
 
 	// read request param
 	var p = r.URL.Query().Get("param")
 	if p != "" {
-		_, _ = w.Write([]byte(" "))
-		_, _ = w.Write([]byte(p))
+		resp.ParamGET = p
 	}
 
 	// read request body
 	var body = m.GetBodyCtx(r.Context())
 	if len(body) > 0 {
-		_, _ = w.Write([]byte(" "))
-		_, _ = w.Write(body)
+		resp.ParamPOST = string(body)
+	}
+
+	msg.Data = resp
+
+	var err = msg.Return(w)
+	if err != nil {
+		log.Printf("write response: %s", err)
 	}
 }
