f6976709105f974a2acc59d5ecce86ac528420be
Andrei Drulchenko <andrdru@gmail.com>
2022-01-31 22:42:46 +0300

    remove body reader middleware

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/rs/zerolog"
@@ -10,10 +12,15 @@ var LoggerHandler = func(r *http.Request, logger *zerolog.Logger) {
 	var l = logger.Info()
 	if (r.Method == http.MethodPost || r.Method == http.MethodPut) &&
 		!isContentTypeMultipart(r) {
-		var b = GetBodyCtx(r.Context())
+		var b, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			logger.Err(err).Msg("could not read request body")
+			return
+		}
 		if b != nil {
 			l = l.Bytes("body", b)
 		}
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(b))
 	}
 
 	l.Msgf("HTTP request: %s %s", r.Method, r.RequestURI)
